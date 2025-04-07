7ada941d4dd4c14d743aa4bc8f0bd84adad1bbfa
ahlesk <lima.alexander.las@gmail.com>
2025-04-07 17:32:45 -0300

    Adicionado estudos de recursão com exemplos e Fibonacci

--- Recursao.c
+++ Recursao.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int fatorial (int n){
     if (n == 0){ //Aqui se concentra o caso base, que dá a condição de resolvido o problema
         return 1;
@@ -12,4 +12,19 @@ int fatorial (int n){
         printf("O resultado do fatorial é %d\n",resultado);
         return 0;
     }
+}
+*/
+// Exemplo usando Fibonacci
+
+int fibonacci (int n){
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2); //Esta é a chamada recursiva que instância os casos
+}
+
+int main(){
+    int n=5;
+    printf("O resultado é de %d\n", fibonacci(n));
+    return 0;
 }
